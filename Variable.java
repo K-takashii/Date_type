a8ed61a400fc9c0123400e28cf3a1178dbe81634
“K-takashii” <nanase.sigre.1627.yorushika@gmail.com>
2021-12-20 00:23:26 +0900

    Initial commit

---
+++ Variable.java
@@ -0,0 +1,12 @@
+public class Variable {
+    public static void main(String[] args) {
+        //整数のデータ型の変数の宣言の時は「int型」を使う。
+        
+        int var; //次行で１を代入するための「var」という変数を宣言する
+
+        var = 1; //変数を代入するときは、イコールを使用する
+
+        System.out.println(var);
+    }
+    
+}
